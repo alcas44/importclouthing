e5b8aae23c36bbe45358016e77b083159a74eb6c
alcas44 <cescmazariegos@gmail.com>
2022-06-27 21:32:01 -0600

    Gracias a Dios Terminado el Ingresos Historiales e Inicio de Inventario Lunes

--- HistorialApp/urls.py
+++ HistorialApp/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('',views.historialcliente,name="HistorialCliente"),
     path('detallecliente/<str:id>',views.detallecliente,name="DetalleCliente"),
+    path('inventario/',views.inventarioarticulos,name="InventarioArticulos"),
 ]
