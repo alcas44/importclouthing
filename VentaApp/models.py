ac5986e6af9618c27abc0035fad4a1fd69889b07
alcas44 <cescmazariegos@gmail.com>
2022-07-01 10:46:35 -0600

    Gracias a Dios Iniciando Modulo Ventas Viernes Mañana

--- VentaApp/models.py
+++ VentaApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.forms import CharField
 
-# Create your models here.
+class DatosVenta(models.Model):
+    venta = models.BigIntegerField(primary_key=True)
+    nit = models.CharField(max_length=14,blank=False)
+    negocio = models.CharField(max_length=50,blank=False)
+    direccion = models.CharField(max_length=500,blank=False)
+    fecha_venta = models.CharField(max_length=9,blank=False)
+    vendedor = models.CharField(max_length=20,blank=False)
+    estado = models.IntegerField(blank=False,default=0)
--- VentaApp/views.py
+++ VentaApp/views.py
@@ -2,17 +2,22 @@ from django.shortcuts import render,redirect
 from IngresosApp.models import Articulos,Clientes
 from datetime import datetime
 
+contador = 0
+
 def iniciarventa(request):
     if not request.user.is_authenticated and not request.user.is_active and request.user.rol == 'admin':
         return redirect('/')
     else:
+        global contador
         if request.method == "POST":
             try:
                 dato = Clientes.objects.filter(nit=request.POST["dato"])
                 fecha = datetime.today()
-                return render(request,"VentaApp/buscar.html",{'dato':dato,'f':fecha})
+                contador = contador + 1 # hacer que aumente despues del save hacer un if para que acumule
+                return render(request,"VentaApp/buscar.html",{'dato':dato,'f':fecha,'c':contador})
             except:
                 dato = "Error"
+                contador = contador - 1
                 return redirect('IniciarVenta')
         else:
             return render(request,"VentaApp/buscar.html")
