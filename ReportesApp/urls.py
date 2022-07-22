6bd2a9dedbad09c916cf7d7911e1c206173acb1d
alcas44 <cescmazariegos@gmail.com>
2022-07-22 10:18:38 -0600

    Gracias a Dios Terminados los Reportes

--- ReportesApp/urls.py
+++ ReportesApp/urls.py
@@ -4,4 +4,9 @@ from django.urls import path
 urlpatterns = [
     path('clientes/<str:id>',views.rclientes,name="ReporteCliente"),
     path('envios/<str:n>',views.renvios,name="ReporteEnvios"),
+    path('reportefectivo/<str:n>',views.refectivo,name="RE"),
+    path('reportecheque/<str:n>',views.rcheque,name="RC"),
+    path('reportetarjeta/<str:n>',views.rtarjeta,name="RT"),
+    path('reportedeposito/<str:n>',views.rdeposito,name="RD"),
+    path('reportecredito/<str:n>',views.rcredito,name="RNC"),
 ]
--- ReportesApp/views.py
+++ ReportesApp/views.py
@@ -1,9 +1,8 @@
-from cgi import print_arguments
-import imp
 from django.shortcuts import render,redirect
 from IngresosApp.models import Clientes,Envios
 from VentaApp.models import DatosVenta,Detalle
 from PagoApp.models import NotaCredito, PagoCheque, PagoDeposito, PagoEfectivo, PagoTarjeta
+from django.db.models import Sum
 
 
 
@@ -14,7 +13,16 @@ def rclientes(request,id):
     else:
         #obtenemos el cliente
         ver = Clientes.objects.get(nit=id)
-        
+
+        #totales
+        totales = Detalle.objects.filter(nit=id).aggregate(tot=Sum('total'))
+        total_efe = PagoEfectivo.objects.filter(nit=id).count()
+        total_tar = PagoTarjeta.objects.filter(nit=id).count()
+        total_che = PagoCheque.objects.filter(nit=id).count()
+        total_dep = PagoDeposito.objects.filter(nit=id).count()
+        total_cre = NotaCredito.objects.filter(nit=id).count()
+
+
         #datos de pago en efectivo
         efectivo = PagoEfectivo.objects.filter(nit=id)
         
@@ -34,7 +42,7 @@ def rclientes(request,id):
 
 
 
-        return render(request,"ReportesApp/clientes.html",{'ver':ver,'vc':vc})
+        return render(request,"ReportesApp/clientes.html",{'ver':ver,'vc':vc,'total':totales,'te':total_efe,'tt':total_tar,'tc':total_che,'td':total_dep,'nc':total_cre,'n':id})
 
 
 
@@ -48,7 +56,86 @@ def renvios(request,n):
       
        
     
-    return render(request,"ReportesApp/envios.html",{'ver':ver})        
+    return render(request,"ReportesApp/envios.html",{'ver':ver})
+
+
+
+
+def refectivo(request,n):
+    if not request.user.is_authenticated and not request.user.is_active and request.user.rol == 'admin':
+        return redirect('/')
+    else:
+        ver = PagoEfectivo.objects.filter(nit=n)
+            #obtenemos el cliente
+        cli = Clientes.objects.get(nit=n)
+      
+       
+    
+    return render(request,"ReportesApp/efectivo.html",{'ver':ver,'cl':cli})
+
+
+
+
+def rtarjeta(request,n):
+    if not request.user.is_authenticated and not request.user.is_active and request.user.rol == 'admin':
+        return redirect('/')
+    else:
+        ver = PagoTarjeta.objects.filter(nit=n)
+            #obtenemos el cliente
+        cli = Clientes.objects.get(nit=n)
+      
+       
+    
+    return render(request,"ReportesApp/tarjeta.html",{'ver':ver,'cl':cli})  
+
+
+
+
+
+def rcheque(request,n):
+    if not request.user.is_authenticated and not request.user.is_active and request.user.rol == 'admin':
+        return redirect('/')
+    else:
+        ver = PagoCheque.objects.filter(nit=n)
+            #obtenemos el cliente
+        cli = Clientes.objects.get(nit=n)
+      
+       
+    
+    return render(request,"ReportesApp/cheque.html",{'ver':ver,'cl':cli})    
+
+
+
+
+
+def rdeposito(request,n):
+    if not request.user.is_authenticated and not request.user.is_active and request.user.rol == 'admin':
+        return redirect('/')
+    else:
+        ver = PagoDeposito.objects.filter(nit=n)
+            #obtenemos el cliente
+        cli = Clientes.objects.get(nit=n)
+      
+       
+    
+    return render(request,"ReportesApp/deposito.html",{'ver':ver,'cl':cli})  
+
+
+
+
+
+def rcredito(request,n):
+    if not request.user.is_authenticated and not request.user.is_active and request.user.rol == 'admin':
+        return redirect('/')
+    else:
+        ver = NotaCredito.objects.filter(nit=n)
+            #obtenemos el cliente
+        cli = Clientes.objects.get(nit=n)
+      
+       
+    
+    return render(request,"ReportesApp/notacredito.html",{'ver':ver,'cl':cli})               
+
 
 
 
